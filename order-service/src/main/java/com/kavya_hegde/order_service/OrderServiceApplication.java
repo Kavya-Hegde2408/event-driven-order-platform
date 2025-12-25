7dacdecba0a67140654645f07eecb59ac52a0997
Kavya Hegde <kavyahegde242@gmail.com>
2025-12-25 12:31:09 +0530

    Bootstrap order service with Spring Boot and Kafka

---
+++ order-service/src/main/java/com/kavya_hegde/order_service/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kavya_hegde.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
