62fb504d7f5653021b981819be303ae3cde89a88
Kavya Hegde <kavyahegde242@gmail.com>
2025-12-30 19:19:54 +0530

    added security

---
+++ api-gateway/src/main/java/com/kavyahegde/api_gateway/filter/JwtAuthFilter.java
@@ -0,0 +1,54 @@
+package com.kavyahegde.api_gateway.filter;
+
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+
+@Component
+public class JwtAuthFilter extends AbstractGatewayFilterFactory<JwtAuthFilter.Config> {
+
+    public JwtAuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return unauthorized(exchange);
+            }
+
+            String authHeader = exchange.getRequest()
+                    .getHeaders()
+                    .getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                return unauthorized(exchange);
+            }
+
+            String token = authHeader.substring(7);
+
+            if (token.isBlank()) {
+                return unauthorized(exchange);
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {
+    }
+}
+
---
+++ api-gateway/src/main/java/com/kavyahegde/api_gateway/security/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.kavyahegde.api_gateway.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    private final Key key;
+
+    public JwtUtil(
+            @Value("${jwt.secret}") String secret
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public void validateToken(String token) {
+        Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token);
+    }
+}
+
+
